4e82a3fadc2e16ebdf83465c884109b79da490e0
Loudovic <loudovic.rexharrison@gmail.com>
2021-07-13 12:56:32 +0200

    Add login and register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('balance');
-            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
